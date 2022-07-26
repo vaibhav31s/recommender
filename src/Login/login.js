79c03a75b7734c6cf9de5cb277c697a4f88b1d67
prateekv2003 <prateekv2003@gmail.com>
2022-07-26 12:21:26 +0530

    login

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,35 +1,26 @@
-import React, { Component } from 'react';
-import './login.css';
+import React, { Component } from "react";
+import "./login.css";
 export class login extends Component {
   render() {
     return (
-        <div class="container">
-        <div class="card">
-            <div class="top-row">
-                <h1>Hello again</h1>
-                <p>Log in with your email and password</p>
-            </div>
-            <div class="card-details">
-                <input type="text" placeholder="Enter your name"/>
-                <i class="fa fa-envelope"></i>
-            </div>
-            <div class="card-details">
-                <input type="password" id="password-input" placeholder="Enter your password"/>
-                <i class="fa fa-lock"></i>
-                <span><small class="fa fa-eye-slash passcode"></small></span>
-                
-            </div>
-            
-               <p class="forget">Forgot your password?</p>
-               
-                   <button class="sign-in">Sign in</button>
-              
-               <p class="sign-up">Dont't have an account?<a href="#"> Sign up</a></p>
-            
+        <div className="container container-login">
+            <div class="wrap">
+            <h1 class="register-title">Welcome</h1>
+            <form action="#">
+                <div class="register-switch">
+                    <input type="radio" name="sex" value="F" id="sex_f" class="register-switch-input" checked/>
+                    <label for="sex_f" class="register-switch-label">Buyer</label>
+                    <input type="radio" name="sex" value="M" id="sex_m" class="register-switch-input"/>
+                    <label for="sex_m" class="register-switch-label">Seller</label>
+                </div>
+                <input type="email" class="register-input" placeholder="Email address"/>
+                <input type="password" class="register-input" placeholder="Password"/>
+                <input type="submit" value="Login" class="register-button"/>
+            </form>
         </div>
-    </div>
-    )
+        </div>
+    );
   }
 }
 
-export default login
+export default login;
