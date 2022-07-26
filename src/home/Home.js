f206a59e4669e7b24abf8c6382ac5984899ba631
prateekv2003 <prateekv2003@gmail.com>
2022-07-26 21:53:30 +0530

    hero

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Search from '../search/Search'
+import Hero from './Hero'
+import Recommended from './Recommended'
 import TopOffers from './TopOffers'
 import Trending from './Trending'
 const Home = () => {
   return (
     <>
+        <Hero/>
         <TopOffers/><hr />
+        <Recommended/><hr />
         <Trending/><hr />
     </>
   )
